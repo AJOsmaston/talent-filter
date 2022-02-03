48bd072197fc839a044daebee6f1e68268b3ab3a
AJOsmaston <dajoxx@gmail.com>
2022-02-03 15:34:41 +0000

    raised allTalent call 1 higher

--- src/components/ResultDisplay.jsx
+++ src/components/ResultDisplay.jsx
@@ -1,20 +1,14 @@
 import TalentCard from "./TalentCard"
 import { useEffect, useState } from 'react'
 
-function ResultDisplay() {
+function ResultDisplay( {allTalent} ) {
 
-  const [allTalent, setAllTalent] = useState();
-  const sampleData = require('../data/exampleData')
-
-  useEffect(() => {
-    setAllTalent(sampleData)
-  }, [sampleData])
-
+  
   if (!allTalent) {
     return <div className="heading">Enter a location to search available Talent</div> 
   }
   
-  return  allTalent && (
+  return allTalent && (
     <div className="display">
       {allTalent.map(talent => (
         <TalentCard 
--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,11 +1,20 @@
 import TalentFilter from "./TalentFilter"
 import ResultDisplay from "./ResultDisplay"
+import { useEffect, useState } from 'react'
 
 function SearchPage() {
+
+  const [allTalent, setAllTalent] = useState();
+  const sampleData = require('../data/exampleData')
+
+  useEffect(() => {
+    setAllTalent(sampleData)
+  }, [sampleData])
+
   return (
     <div className="homePage">
       <TalentFilter />
-      <ResultDisplay />
+      <ResultDisplay allTalent={allTalent} />
     </div>
   )
 }
